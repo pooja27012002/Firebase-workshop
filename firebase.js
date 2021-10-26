4a1d36cdf21078df57f9fe40d3707ee77fc54f6c
pooja27012002 <92859828+pooja27012002@users.noreply.github.com>
2021-10-26 18:24:53 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC72NK8ooN7M-yatiZWcj9FaujGgdRdisI",
+    authDomain: "workshop-project-login-form.firebaseapp.com",
+    projectId: "workshop-project-login-form",
+    storageBucket: "workshop-project-login-form.appspot.com",
+    messagingSenderId: "1059406378504",
+    appId: "1:1059406378504:web:12ebe19ffcb5b07b9d03bd"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
